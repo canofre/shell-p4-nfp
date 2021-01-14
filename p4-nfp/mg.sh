02bf868563e07e9463e99cf3d46bbc8ff390d708
canofre <canofre@inf.ufsm.br>
2021-01-14 09:02:38 -0300

    add exec moogen

---
+++ p4-nfp/mg.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+##
+## R.Canofre - canofre@inf.ufsm.br
+## Script para gernciar a inicializacao do ambiente, carregando o MoonGem 
+## e atribuindo a interface ao DPDK
+##
+
+# Define diretorio onde esta o MoonGen e o dpdk-devbind
+PATH_MG=/opt/MoonGen
+PATH_EX=$PATH_MG/build/MoonGen
+PATH_USERTOOLS=$PATH_MG/libmoon/deps/dpdk/usertools
+
+main() {
+    case "$1" in
+        1|pkt)
+            packetgen $* ;;
+        *)
+            uso ;;
+    esac
+}
+
+uso(){
+    printf " USO $0 [opcoes]\n\n"
+    printf " Opcoes:\n"
+    printf "\t 1|pkt	: 1 (tx) (rx) (IP) - executa packetgen da netronome \n"
+}
+
+packetgen(){
+	$PATH_EX $PATH_MG/examples/netronome-packetgen/packetgen.lua \
+		-tx $1 -rx $2 --dst-ip $3 --dst-ip-vary 0.0.0.0
+}
+
+main $*
+
