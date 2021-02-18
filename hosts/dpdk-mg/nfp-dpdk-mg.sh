a5babbcf7c74974bd285b42065aebc6c9b46344b
canofre <canofre@inf.ufsm.br>
2021-02-18 16:14:14 -0300

    fix

--- hosts/dpdk-mg/nfp-dpdk-mg.sh
+++ hosts/dpdk-mg/nfp-dpdk-mg.sh
@@ -1,18 +1,12 @@
 #!/bin/bash
 ##
 ## R.Canofre - canofre@inf.ufsm.br
-## Script para gernciar a inicializacao do ambiente, carregando o MoonGem 
-## e atribuindo a interface ao DPDK
+## Script to manage moongen startup ans assign interfaces to dpdk
 ##
 
-# Define diretorio onde esta o MoonGen e o dpdk-devbind
+# Defines the path to the moongen and usertools directory
 PATH_MG=/opt/MoonGen
 PATH_USERTOOLS=$PATH_MG/libmoon/deps/dpdk/usertools
-DPDK_DRIVER=igb_uio
-
-# Inicializa a interface e seu driver de kernet
-#declare -A NFP=( [idt]=03:00.0 [driver]=nfp ) #enp8s0np...
-#declare -A PCI=( [idt]=04:00.0 [driver]=r8169 ) #enp6s0
 
 function main() {
     case "$1" in
@@ -45,8 +39,7 @@ function printHelp(){
     printf "\t\t DRIVER: igb_uio - DPDK | nfp - Netronome \n"
 }
 
-## Lista as informações das interfaces
-## [IDT da placa] 'Descricao' [if=..] drv=em_uso unused=nao_usado
+## Lists information about interfaces
 function nicStatus(){
     $PATH_USERTOOLS/dpdk-devbind.py --status-dev net | cut -d: -f2,3   
 }
@@ -61,13 +54,17 @@ function nicBind(){
     $PATH_USERTOOLS/dpdk-devbind.py --bind=$3 $2
 } 
 
-# Carrega o MoonGen 
+# Initializes moongen and add the corret interfaces to dpdk
 function moongenInit(){
-    #$PATH_MG/build.sh
-    idRemover=`nicStatus | grep drv=igb_uio | grep -v 4000 | cut -d" " -f1`
+    $PATH_MG/build.sh
+    # removes all interfaces from the dpdk
+    idRemover=`nicStatus | grep drv=igb_uio | cut -d" " -f1`
     for((i=0;i<${#idRemover[*]};i++)); do
-        echo "nicUnbind $i ${idRemover[$i]}"
+        nicUnbind $i ${idRemover[$i]}
     done
+    # adds the Netronome interface to dpdk
+    idAdd=`nicStatus | grep 4000 | cut -d" " -f1`
+    nicBind 0 $idAdd igb_uio
 }
 
 main $*
