e556dab502ad084051a59d97e9865ff85f855963
canofre <canofre@inf.ufsm.br>
2021-05-03 16:50:45 -0300

    mount huge pages

--- hosts/dpdk-mg/nfp-dpdk-mg.sh
+++ hosts/dpdk-mg/nfp-dpdk-mg.sh
@@ -13,6 +13,12 @@ function main() {
         mi|moongen-init)
             moongenInit > /tmp/moongen.log
             ;;
+        ms|moongen-status)
+            service dpdk-mg status
+            ;;
+        hp|mount-hp)
+            mount -t hugetlbfs none /mnt/huge/ -o pagesize=1G
+            ;; 
         ns|nic-status)
             nicStatus $*
             ;;
@@ -31,10 +37,12 @@ function main() {
 function printHelp(){
     printf " USO nfp-dpdk-mg.sh [opcoes]\n\n"
     printf " Opcoes:\n"
-    printf "\t h                      : exibe opções e uso\n"
-    printf "\t mi|moongen-init        : incializa o moongen \n"
-    printf "\t ns|nic-status          : lista o status das NIC \n"
-    printf "\t nu|nic-unbind IDT      : desarrega o driver de uma NIC \n"
+    printf "\t h                    : exibe opções e uso\n"
+    printf "\t hp|mount-hugepages   : monta hugepagens 1G e /mnt/hugei\n"
+    printf "\t mi|moongen-init      : incializa o moongen \n"
+    printf "\t ms|moongen-status    : status de inicializacao do moongen \n"
+    printf "\t ns|nic-status        : lista o status das NIC \n"
+    printf "\t nu|nic-unbind IDT    : desarrega o driver de uma NIC \n"
     printf "\t nb|nic-bind IDT DRIVER : carrega um driver em uma NIC \n"
     printf "\t\t DRIVER: igb_uio - DPDK | nfp - Netronome \n"
 }
